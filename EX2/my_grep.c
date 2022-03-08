7976ed1d28189b57fd5b11a5592e4c0d588a2a5b
Yaniv Blum <jungob94@gmail.com>
2022-03-08 18:11:19 +0200

    works

--- EX2/my_grep.c
+++ EX2/my_grep.c
@@ -5,7 +5,7 @@
 
 void validate_args(int args) {
 	if (args != 3) {
-		fprintf(stderr, "there are %d arguments but it shuold be 3", args);
+		printf("there are %d arguments but it shuold be 3", args);
 		exit(-1);
 	}
 	return;
@@ -15,7 +15,6 @@ bool exp_in_line(char* line, char* exp) {
 	char* search_ptr = line;
 	int lim = strlen(line) - strlen(exp);
 	int count = 0;
-
 	while ((*search_ptr != '\0') && (count <= lim)) {
 		if (strncmp(search_ptr, exp, strlen(exp)) == 0)
 			return true;
@@ -28,29 +27,25 @@ bool exp_in_line(char* line, char* exp) {
 int main(int argc, char* argv[]) {
 	FILE* input_file = NULL;
 	char* line_buf = NULL;
-	char* file_name, exp;
 	size_t line_buf_len = 0;
 	int line_len;
 
 	validate_args(argc);
-	file_name = argv[2];
-	exp = argv[1];
 
-	input_file = fopen(file_name, "r");
+	input_file = fopen(argv[2], "r");
 	if (input_file == NULL) {
-		fprintf(stderr, "error in open file");
+		printf("error in open file");
 		return -1;
 	}
 
 	line_len = getline(&line_buf, &line_buf_len, input_file);
 	while (line_len != -1) {
-		if (exp_in_line(line_buf, exp)) {
-			fprintf(stdout, "%s", line_buf);
+		if (exp_in_line(line_buf, argv[1])) {
+			printf("%s", line_buf);
 		}
 		line_len = getline(&line_buf, &line_buf_len, input_file);
 	}
-
-	free(line_buf); free(exp); free(file_name);
+	free(line_buf);
 	fclose(input_file);
 	return 0;
 }
